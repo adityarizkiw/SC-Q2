86f8bc7fc12e308f8d2b9583a708aa3f3bdbc5cd
adityarizkiw <arizkiwidyanto@gmail.com>
2023-10-23 07:09:25 +0700

    Menambahkan fungsi perkalian dan pembagian

--- Math_function.py
+++ Math_function.py
@@ -1,3 +1,12 @@
 
 def add(a, b):
     return a + b
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Tidak bisa dibagi oleh nol"
